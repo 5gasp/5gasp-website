f290c15767fcf007b45d1281005ce1a03a91c6f2
Rafael Direito <rafael.neves.direito@ua.pt>
2021-04-10 12:28:18 +0100

    updated ci/cd website api

--- cicd_api/app/__init__.py
+++ cicd_api/app/__init__.py
@@ -4,11 +4,12 @@ from datetime import datetime
 
 class Root(object):
     # key
-    key= 'xxx'
+    key= 'pE9VbS@JWGyK:,K:'
     git_repo_path = 'xxx'
     # jekyll
-    site_output_base_path = 'xxx'
-    site_output_dir_name = 'xxx'
+    site_output_base_path = '/Users/rd/Desktop/UA/UA_9_Semestre/5GASP/Website/cicd_api'
+    site_output_zip_name = 'website.zip'
+    site_output_dir_name = 'website'
     # nginx
     nginx_source_dir = 'xxx'
     served_dir_name = 'xxx'
@@ -17,37 +18,54 @@ class Root(object):
 
 
     post_commands = [
-        (f"cd {git_repo_path} && git pull", "Couldn't pull the repository."),
-        (f"cd {git_repo_path} && jekyll build -d {os.path.join(site_output_base_path, site_output_dir_name)}", "Couldn't build the jekyll project."),
-        (f"rm -rf {os.path.join(nginx_source_dir, site_output_dir_name)} && mv {os.path.join(site_output_base_path, site_output_dir_name)} {nginx_source_dir}", "Couldn't move the output website to nginx dir."),
-        (f"cd {nginx_source_dir} && rm -rf {backup_dir_name} && mv {served_dir_name} {backup_dir_name}", "Couldn't generate backup of the last version."),
-        (f"cd {nginx_source_dir} && mv {site_output_dir_name} {served_dir_name}", "Couldn't update the directory that is being served."),
-        (f"echo {str(datetime.now())} > {os.path.join(nginx_source_dir, served_dir_name, timestamp_file)}", "Couldn't update the timestamp.")
-    ]   
+            (f"rm -rf {os.path.join(site_output_base_path, site_output_dir_name)} ", "Couldn't remove the genereated website dir."),
+            (f"unzip {os.path.join(site_output_base_path, site_output_zip_name)} -d {os.path.join(site_output_base_path, site_output_dir_name)}", "Couldn't unzip website to nginx dir."),
+            (f"rm -rf {os.path.join(nginx_source_dir, site_output_dir_name)} && mv {os.path.join(site_output_base_path, site_output_dir_name)} {nginx_source_dir}", "Couldn't move the output website to nginx dir."),
+            (f"cd {nginx_source_dir} && rm -rf {backup_dir_name} && mv {served_dir_name} {backup_dir_name}", "Couldn't generate backup of the last version."),
+            (f"cd {nginx_source_dir} && mv {site_output_dir_name} {served_dir_name}", "Couldn't update the directory that is being served."),
+            (f"echo $(date) > {os.path.join(nginx_source_dir, served_dir_name, timestamp_file)}", "Couldn't update the timestamp.")
+    ]
 
     @cherrypy.expose
-    @cherrypy.tools.json_in()
-    def index(self):
+    def index(self, key=None, website_zip=None):
+        print(cherrypy.request.method)
         cherrypy.request.show_tracebacks = False
         if  cherrypy.request.method == 'POST':
+            print("POST")
             # get key
-            try:
-                input_json = cherrypy.request.json
-                key = input_json["key"]
-            except:
+            if not key:
                 return "No key received"
 
             if key != self.key:
                 return "The key is incorrect"
 
-            # run all commands
+
+            # read file content
+            try:
+                whole_data = bytearray()
+                while True:
+                    data = website_zip.file.read(8192)
+                    whole_data += data # Save data chunks in ByteArray whole_data
+
+                    if not data:
+                        break
+
+                written_file = open(os.path.join(self.site_output_base_path, self.site_output_zip_name), "wb") # open file in write bytes mode
+                written_file.write(whole_data) # write file
+            except Exception as e:
+                print(e)
+                return "Couldn't read the uploaded file"
+
+            # deploy in nginx
             for command, error in self.post_commands:
                 if os.system(command) != 0:
                     return error
+
             return "Updated"
-        
+
         elif cherrypy.request.method == 'GET':
-                try:    
+            print("GET")
+            try:
                 f = open(os.path.join(self.nginx_source_dir, self.served_dir_name, self.timestamp_file))
                 update = f.readline()
                 return "Last Update: " + update
--- cicd_api/wsgi.py
+++ cicd_api/wsgi.py
@@ -7,7 +7,8 @@ if __name__=='__main__':
 
     cherrypy.config.update({
         'server.socket_host': '0.0.0.0',
-        'server.socket_port': 8080,
+        'server.socket_port': 9000,
+        'server.max_request_body_size' : 157286400,
     })
 
     # Run the application using CherryPy's HTTP Web Server
